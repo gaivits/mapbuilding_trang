01cf8d273a82b71e3fa4d8809f637fe2898e64f6
gaivits <49124734+gaivits@users.noreply.github.com>
2024-08-27 18:57:20 +0700

    Update config.php

--- service/config.php
+++ service/config.php
@@ -26,7 +26,7 @@ header('Content-Type: text/html; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
 
 $conn = pg_connect("host=$servername port=$port dbname=$dbname user=$username password=$password") or die('Error: ' . pg_last_error());
-echo $conn;
+//echo $conn;
 
 ?>
 
